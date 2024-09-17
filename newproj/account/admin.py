5691d2fd40e282b5398131369b81cf3fee5ab8b7
gauravghale051 <gauravghale051@gmail.com>
2024-09-17 13:18:43 +0545

    first commit

---
+++ newproj/account/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+User = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserAdminCreationForm
+    form = UserAdminChangeForm
+    model = User
+    list_display = ("email", "is_active",)
+    list_filter = ("email", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ( "is_active","staff","admin","customer","driver")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password", "password_2", "staff","admin","customer","driver",
+                "is_active"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
